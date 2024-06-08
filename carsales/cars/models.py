bb7fb60639bbc2ee6f3bbe2c8a7a33e2d478a86d
Yabiti <yabetseyohannes@gmail.com>
2024-06-08 15:37:33 +0300

    .

--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class carsale(models.Model):
     Brand = models.CharField(max_length=25 ,blank=False, null=True),
     Models = models.CharField(max_length=25, blank=False, null=True)
+
+    def __str__(self):
+        return "{sel}"
