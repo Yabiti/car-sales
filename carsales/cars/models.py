56b0cbfb4c4b9f11ba81bb2241a0a375589372ce
Yabiti <yabetseyohannes@gmail.com>
2024-06-08 15:33:46 +0300

    .

--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class carsale(models.Model):
-    Brand = models.CharField(max_length=25, blank=False null=True)
-    Models = models.CharField
+    Brand = models.CharField(max_length=25 blank=False null=True)
+    Models = models.CharField(max_length=25 blank=False null=True)
