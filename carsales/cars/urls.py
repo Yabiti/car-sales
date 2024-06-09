ae8aabc1df333aaa650941c52a5fd8dcb4b01d38
Yabiti <yabetseyohannes@gmail.com>
2024-06-09 13:05:54 +0300

    .

--- carsales/cars/urls.py
+++ carsales/cars/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
+    path("", views.home, name="home"), 
     path("home/", views.home, name="home"),
     path("insertuser", views.insertuser, name="insertuser"),
     path("about/", views.about, name="about"),
