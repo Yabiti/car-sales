74989bfa39dbeb1bcf2211db67667f132fc20922
Yabiti <yabetseyohannes@gmail.com>
2024-06-10 01:36:55 +0300

    .

--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -6,5 +6,13 @@ class carsale(models.Model):
     Brand = models.CharField(max_length=25, blank=True, null=False)
 
 
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
     class Meta:
         db_table = "carsale"
