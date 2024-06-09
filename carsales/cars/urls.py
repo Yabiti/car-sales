691c80bed6c7d4fb66fa7fc388d040035ff94988
Yabiti <yabetseyohannes@gmail.com>
2024-06-09 13:14:09 +0300

    .

--- carsales/cars/urls.py
+++ carsales/cars/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"), 
     path("home/", views.home, name="home"),
-    path("insertuser", views.insertuser, name="insertuser"),
+    path("insertuser/", views.insertuser, name="insertuser"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact")
 ]
