87831091b17405b3aebdddeecf7a4d6ad33a0b30
Yabiti <yabetseyohannes@gmail.com>
2024-06-10 09:04:21 +0300

    .

--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -8,22 +8,6 @@ class carsale(models.Model):
     model = models.CharField(max_length=25, blank=True, null=False)
     brand = models.CharField(max_length=25, blank=True, null=False)
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    caption = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        if self.pk is None:  # Image is being created
-            super().save(*args, **kwargs)
-            img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-        else:  # Image is being updated
-            super().save(*args, **kwargs)
-
-
+    
     class Meta:
         db_table = "carsales"
