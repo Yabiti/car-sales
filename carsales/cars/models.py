5dcbba73446fe6967ddc4c7815f8132ff470091b
Yabiti <yabetseyohannes@gmail.com>
2024-06-10 09:24:54 +0300

    .

--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -4,3 +4,9 @@ from django.db import connection
 
 # Create your models here.
 
+class carsale(models.Model):
+    brand = models.CharField(max_length=25, blank=True, null=False),
+    model = models.CharField(max_length=25, blank=True, null=False),
+
+    class Meta:
+        db_table = "carsale"
