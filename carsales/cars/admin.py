3e5e3fd5a5fdfa4c98cc52a089e187772517740d
Yabiti <yabetseyohannes@gmail.com>
2024-06-10 09:22:25 +0300

    .

--- carsales/cars/admin.py
+++ carsales/cars/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import carsale
-from .models import Image
 
 admin.site.register(carsale)
--- carsales/cars/models.py
+++ carsales/cars/models.py
@@ -4,10 +4,3 @@ from django.db import connection
 
 # Create your models here.
 
-class carsale(models.Model):
-    model = models.CharField(max_length=25, blank=True, null=False)
-    brand = models.CharField(max_length=25, blank=True, null=False)
-
-    
-    class Meta:
-        db_table = "carsale"
